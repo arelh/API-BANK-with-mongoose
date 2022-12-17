90e01cbcf737ceee868bef9583ad0b48b06ae7f9
arel hagag <areltv297@gmail.com>
2022-12-17 22:40:01 +0200

    .

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,15 +1,21 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config()
 
-// const URL= `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS} @cluster0.n4xz03i.mongodb.net/?retryWrites=true&w=majority`
 
-const URL= `mongodb+srv://jon:yaEh0kJ2ARdyokrv @cluster0.n4xz03i.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.connect(URL,(err,mongoDbInstance)=>{
-    if(err){
-        throw Error("mongoDB connection error:"+err)
-    }
-    const {host,port,name}=mongoDbInstance
-    console.log({host,port,name});
-})
+  import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+// const URL= `mongodb+srv://jon:yaEh0kJ2ARdyokrv @cluster0.n4xz03i.mongodb.net/?retryWrites=true&w=majority`
+
+
+const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.n4xz03i.mongodb.net/?retryWrites=true&w=majority`
+;
+
+mongoose.connect(URL, (err, mongoDbInstance) => {
+  if (err) {
+    throw Error('MongoDB connection error: ' + err);
+  }
+  const { host, port, name } = mongoDbInstance;
+  console.log({ host, port, name });
+});
