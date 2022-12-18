13d9542e9be2582ef2d5a070217ac1bbf6211ecf
arel hagag <areltv297@gmail.com>
2022-12-18 13:47:29 +0200

    backend

--- server/controllers/Users.controller.js
+++ server/controllers/Users.controller.js
@@ -1,39 +1,94 @@
-import { BankUsers} from "../models/user.models.js"
+import { BankUsers } from "../models/user.models.js";
 // import { addUserToMongo } from "../services/users.mongoose.js";
 
-export const getUser=  (req,res)=>{
-    try{
-         res.send('Hola');
-        // const users= BankUsers.find({})
-        // res.status(200).send(users);
-    }catch(err){
-        res.send(err.message)
-    }
-}
-export const addUser= async (req,res)=>{
-    try{
-    const body=req.body
-    const newUser=await BankUsers.create(body)
+export const getUser = async (req, res) => {
+  try {
+    const users = await BankUsers.find({});
+    res.status(200).send(users);
+  } catch {}
+};
+
+export const addUser = async (req, res) => {
+  try {
+    const body = req.body;
+    const newUser = await BankUsers.create(body);
     res.status(201).send(newUser);
-    
-    }catch(err){
-        res.send(err.message)
-    }
-}
+  } catch (err) {
+    res.send(err.message);
+  }
+};
 
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToDelete = await BankUsers.findByIdAndDelete(id);
+    res.status(200).send(userToDelete);
+  } catch (err) {
+    res.send(err.message);
+  }
+};
 
+export const editUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToEdit = await BankUsers.findByIdAndUpdate(
+      id,
+      { name: req.body.name, cash: req.body.cash, credit: req.body.credit },
+      { new: true }
+    );
 
-export const deleteUser= (req,res)=>{
-    try{
-        res.send("hola")
-    }catch(err){
-        res.send(err.message)
-    }
-}
-export const editUser= (req,res)=>{
-    try{
-        res.send("hola")
-    }catch(err){
-        res.send(err.message)
-    }
-}
+    res.status(200).send(userToEdit);
+  } catch (err) {
+    res.send(err.message);
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userById = await BankUsers.findById(id);
+    res.status(200).send(userById);
+  } catch (err) {
+    res.send(err.message);
+  }
+};
+
+export const deposite = async (req, res) => {
+  const { id } = req.params;
+  const depositCash = +req.body.cash;
+  try {
+    let userToDeposite = await BankUsers.findById(id);
+    userToDeposite = await BankUsers.findByIdAndUpdate(
+      id,
+      { cash: userToDeposite.cash + depositCash },
+      { new: true }
+    );
+
+    res.status(200).send(userToDeposite);
+  } catch (err) {
+    res.send(err.message);
+  }
+};
+
+export const transferCash = async (req, res) => {
+  const { firstId } = req.params;
+  const { secondId } = req.params;
+  const moneyToTransfer = +req.body.cash;
+  try {
+    let userPass = await BankUsers.findById(firstId);
+    let userGet = await BankUsers.findById(secondId);
+    userPass = await BankUsers.findByIdAndUpdate(
+      firstId,
+      { cash: userPass.cash - moneyToTransfer },
+      { new: true }
+    );
+    userGet = await BankUsers.findByIdAndUpdate(
+      secondId,
+      { cash: userGet.cash + moneyToTransfer },
+      { new: true }
+    );
+    res.status(200).send(userPass);
+  } catch (err) {
+    res.send(err.message);
+  }
+};
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { getUser,addUser,deleteUser,editUser } from "../controllers/Users.controller.js";
+import { getUser,addUser,deleteUser,editUser,getUserById, deposite,transferCash } from "../controllers/Users.controller.js";
 
  export const indexRouter=Router()
 
  indexRouter.get("/users",getUser)
+ indexRouter.get("/users/:id",getUserById)
  indexRouter.post("/users/add",addUser)
- indexRouter.delete("/users/delete",deleteUser)
- indexRouter.post("/users/edit",editUser)
+ indexRouter.post("/users/transfer/:firstId/to/:secondId",transferCash)
+ indexRouter.delete("/users/delete/:id",deleteUser)
+ indexRouter.patch("/users/edit/:id",editUser)
+ indexRouter.patch("/users/deposite/:id",deposite)
 
