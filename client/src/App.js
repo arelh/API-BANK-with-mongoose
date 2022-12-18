90b54abc3948596cc633ef0a1df5a7ff7afaaf44
arel hagag <areltv297@gmail.com>
2022-12-19 00:37:10 +0200

    @

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import NavBar from "./components/navBar.js";
+import Spinner from "./components/Spinner";
 
 function App() {
+  const [info, setInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(
+          `https://mybank-with-mongoose.onrender.com/myBank/Users`
+        );
+        console.log(data);
+        setInfo(data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log(e);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      {console.log(info)}
+      {info.map((user) => {
+        return (
+          <div key={user._id}>
+            <div className="card">
+            <p>{user.name}</p>
+            <p>{user.cash}</p>
+            <p>{user.credit}</p>
+            </div>
+          </div>
+        );
+      })}
+      {isLoading && <Spinner />}
+      
     </div>
   );
 }
---
+++ client/src/components/navBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div className="navBar">
+         welcome
+        </div>
+      );
+}
+
+export default NavBar
